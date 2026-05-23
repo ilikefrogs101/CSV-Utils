2c97b294dfd2e930ced12fa01104c29c02f92df8
ilikefrogs101 <87593516+ilikefrogs101@users.noreply.github.com>
2026-05-23 16:24:57 +1000

    feature: correlationinator does APA7 style now

--- Correlationinator.py
+++ Correlationinator.py
@@ -1,7 +1,8 @@
+import subprocess
 import pandas as pd
 from scipy.stats import pearsonr
 
-df = pd.read_csv("Data/FilteredData/High.csv", sep="&")
+df = pd.read_csv("Data/FilteredData/Low.csv", sep="&")
 
 df["familyPlanning"] = pd.to_numeric(df["familyPlanning"], errors="coerce")
 df["tfr"] = pd.to_numeric(df["tfr"], errors="coerce")
@@ -10,7 +11,16 @@ clean_df = df.dropna(subset=["familyPlanning", "tfr"])
 
 l1 = clean_df["familyPlanning"]
 l2 = clean_df["tfr"]
+
 r, p_value = pearsonr(l1, l2)
 
-print(f"Correlation: {r}")
-print(f"P-value: {p_value}\n")
+n = len(clean_df)
+
+if p_value < .001:
+    p_text = "p < .001"
+else:
+    p_text = f"p = {p_value:.3f}"
+
+r_text = f"r({n - 2}) = {r:.2f}, {p_text}.".replace("0.", ".")
+
+print(r_text)
